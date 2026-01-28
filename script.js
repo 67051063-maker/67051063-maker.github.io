2f1eb0dc2e1e6adceec6c130d9818a0d7db80590
67051063-maker <67051063@kmitl.ac.th>
2026-01-28 22:15:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-function showAlert(){
-    alert("Alert Massage")
-}
+const card = document.getElementById('profileCard');
+
+document.addEventListener('mousemove', (e) => {
+    let x = (window.innerWidth / 2 - e.pageX) / 30;
+    let y = (window.innerHeight / 2 - e.pageY) / 30;
+    card.style.transform = `rotateY(${x}deg) rotateX(${y}deg)`;
+});
